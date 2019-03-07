1796d96180056e189041d83c5c670053c42e740f
Krystian Bąkowski <krystian.bakowskiv8@wp.pl>
2019-03-07 19:25:37 +0100

    add AnimatedSprite and test in menu

---
+++ Pac-man/Pac-man/AnimatedSprite.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pac_man
+{
+    public class AnimatedSprite
+    {
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private int currentFrame;
+        private int totalFrames;
+        private int frameSwitcher;
+       
+        public AnimatedSprite(Texture2D texture, int rows, int columns)
+        {
+            frameSwitcher = 0;
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows * Columns;
+        }
+
+        public void Update()
+        {
+
+            if ((++frameSwitcher % 5) == 0)
+            {
+                currentFrame++;
+                frameSwitcher = 0;
+            }
+            if (currentFrame == totalFrames)
+            {
+                currentFrame = 0;
+            }
+        }
+
+        public void Draw(Vector2 location)
+        {
+            int width = Texture.Width / Columns;
+            int height = Texture.Height / Rows;
+            int row = (int)((float)currentFrame / (float)Columns);
+            int column = currentFrame % Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, (int)((float)width * 0.75), (int)((float)height * 0.75));
+
+            Globals.spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+    }
+}
--- Pac-man/Pac-man/Menu.cs
+++ Pac-man/Pac-man/Menu.cs
@@ -11,11 +11,14 @@ namespace Pac_man
     class Menu : StateTemplate
     {
 
-        Texture2D menutexture;
+        Texture2D menutexture,packman;
+        AnimatedSprite animated;
 
         public Menu()
         {
             menutexture= Globals.contentManager.Load<Texture2D>("menuback");
+            packman = Globals.contentManager.Load<Texture2D>("pac");
+            animated = new AnimatedSprite(packman, 1, 3);
         }
         private enum menu_state  {
             PLAY_GAME,
@@ -26,13 +29,14 @@ namespace Pac_man
 
         public override void Update(GameTime gameTime)
         {
-            
+            animated.Update();
             Draw();  
         }
         public override void Draw()
         {
             Globals.spriteBatch.Begin();
             Globals.spriteBatch.Draw(menutexture, new Rectangle(0, 0, 600, 600), Color.White);
+            animated.Draw(new Vector2(300, 300));
             Globals.spriteBatch.End();
 
 
